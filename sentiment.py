62cfbe0926444cb5a53caad32dce4095712051d7
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-09 10:32:26 +0545

    classifiy

--- sentiment.py
+++ sentiment.py
@@ -12,3 +12,21 @@ for source, filepath in filepath_dict.items():
 
 df = pd.concat(df_list,ignore_index=True)
 print(df.iloc[33])
+
+for source in df['source'].unique():
+    df_source = df[df['source'] == source]
+    sentences = df_source['sentence'].values
+    y = df_source['label'].values
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(
+        sentences, y, test_size=0.25, random_state=1000)
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(sentences_train)
+    X_train = vectorizer.transform(sentences_train)
+    X_test  = vectorizer.transform(sentences_test)
+
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+    print('Accuracy for {} data: {:.4f}'.format(source, score))
